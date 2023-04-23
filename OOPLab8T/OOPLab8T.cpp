8d489e7168357e011925a8b3f177331a5d054c10
Shkliar Maksym <99794346+CociCoc@users.noreply.github.com>
2023-04-23 20:56:40 +0300

    EX 2

--- OOPLab8T/OOPLab8T.cpp
+++ OOPLab8T/OOPLab8T.cpp
@@ -1,100 +1,87 @@
 ﻿// OOPLab8T.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
-#include <iostream>
-#include "example.h"
-int main()
-{
-    int r;       
-    std::cout << "Lab 8 \n";
-     std::cout << " Run example press 1 \n";
-     std::cin >> r; if (r == 1) example();
-    #include <iostream>
-#include <cstring>
- 
 template<typename T>
-void removeMin(T* array, int size) {
-    T minValue = array[0];
-    for (int i = 1; i < size; i++) {
-        if (array[i] < minValue) {
-            minValue = array[i];
+void mergeArrays(T* arr1, int size1, T* arr2, int size2, T* result, int& sizeResult) {
+    std::unordered_set<T> seenValues;
+    
+    for (int i = 0; i < size1; i++) {
+        if (seenValues.find(arr1[i]) == seenValues.end()) {
+            seenValues.insert(arr1[i]);
+            result[sizeResult] = arr1[i];
+            sizeResult++;
         }
     }
     
-    int numMinValues = 0;
-    for (int i = 0; i < size; i++) {
-        if (array[i] == minValue) {
-            numMinValues++;
+    for (int i = 0; i < size2; i++) {
+        if (seenValues.find(arr2[i]) == seenValues.end()) {
+            seenValues.insert(arr2[i]);
+            result[sizeResult] = arr2[i];
+            sizeResult++;
         }
     }
-    
-    T* newArray = new T[size - numMinValues];
-    int newIndex = 0;
-    for (int i = 0; i < size; i++) {
-        if (array[i] != minValue) {
-            newArray[newIndex] = array[i];
-            newIndex++;
-        }
-    }
-    
-    memcpy(array, newArray, (size - numMinValues) * sizeof(T));
-    delete[] newArray;
 }
  
 // Специфікація для типу char*
-void removeMin(char** array, int size) {
-    char* minValue = array[0];
-    for (int i = 1; i < size; i++) {
-        if (strcmp(array[i], minValue) < 0) {
-            minValue = array[i];
+void mergeArrays(char** arr1, int size1, char** arr2, int size2, char** result, int& sizeResult) {
+    std::unordered_set<std::string> seenValues;
+    
+    for (int i = 0; i < size1; i++) {
+        if (seenValues.find(std::string(arr1[i])) == seenValues.end()) {
+            seenValues.insert(std::string(arr1[i]));
+            result[sizeResult] = arr1[i];
+            sizeResult++;
         }
     }
     
-    int numMinValues = 0;
-    for (int i = 0; i < size; i++) {
-        if (strcmp(array[i], minValue) == 0) {
-            numMinValues++;
+    for (int i = 0; i < size2; i++) {
+        if (seenValues.find(std::string(arr2[i])) == seenValues.end()) {
+            seenValues.insert(std::string(arr2[i]));
+            result[sizeResult] = arr2[i];
+            sizeResult++;
         }
     }
-    
-    char** newArray = new char*[size - numMinValues];
-    int newIndex = 0;
-    for (int i = 0; i < size; i++) {
-        if (strcmp(array[i], minValue) != 0) {
-            newArray[newIndex] = array[i];
-            newIndex++;
-        }
-    }
-    
-    for (int i = 0; i < size; i++) {
-        delete[] array[i];
-    }
-    delete[] array;
-    
-    array = newArray;
 }
  
 int main() {
-   							 // Приклад використання для типу int
-    int intArray[] = {2, 5, 1, 3, 1, 7, 1};
-    int intArraySize = sizeof(intArray) / sizeof(int);
-    removeMin(intArray, intArraySize);
-    for (int i = 0; i < intArraySize - 3; i++) {
-        std::cout << intArray[i] << " ";
+    // Приклад використання для типу int
+    int arr1[] = {1, 2, 3, 4, 5};
+    int arr2[] = {4, 5, 6, 7, 8};
+    int size1 = sizeof(arr1) / sizeof(int);
+    int size2 = sizeof(arr2) / sizeof(int);
+    int resultSize = 0;
+    int* result = new int[size1 + size2];
+    
+    mergeArrays(arr1, size1, arr2, size2, result, resultSize);
+    
+    for (int i = 0; i < resultSize; i++) {
+        std::cout << result[i] << " ";
     }
     std::cout << std::endl;
     
-  						  // Приклад використання для типу char*
-    char* strArray[] = {"glue", "pencil”, "pencil-box", "pen", "diary”, "calculator”;
-    int strArraySize = sizeof(strArray) / sizeof(char*);
-    removeMin(strArray, strArraySize);
-    for (int i = 0; i < strArraySize - 2; i++) {
-        std::cout << strArray[i] << " ";
-    }
-    std::cout << std::endl;
+    delete[] result;
+    
+    // Приклад використання для типу char*
+    char* strArr1[] = {"apple", "orange", "banana"};
+    char* strArr2[] = {"banana", "peach", "kiwi"};
+    int strSize1 = sizeof(strArr1) / sizeof(char*);
+    int strSize2 = sizeof(strArr2) / sizeof(char*);
+    int strResultSize = 0;
+    char** strResult = new char*[strSize1 + strSize2];
+    
+    mergeArrays(strArr1, strSize1, strArr2, strSize2, strResult, strResultSize);
+    
+    for (int i = 0; i < strResultSize; i++) {
+        std  ::cout << strResult[i] << " ";
+}
+std::cout << std::endl;
+ 
+for (int i = 0; i < strResultSize; i++) {
+    delete[] strResult[i];
+}
+delete[] strResult;
+ 
 return 0;
 }
 
-
-}
 
